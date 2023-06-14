027f933ca1affd5a81379a688ac8a27845fce6d9
Hyan Ferreira <hcgamesmg2016@gmail.com>
2023-06-14 11:43:12 -0300

    Atividade Django

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+from django.template.response import TemplateResponse
+
+def index(request):
+    # return HttpResponse('Olá Django - index')
+    return TemplateResponse(request, 'index.html', {})
+
+def ola(request):
+    return HttpResponse('Olá Django')
