ce8886651a5ce8f563756f2e008c9b56821afa41
Hyan Ferreira <114369661+HyanFerreira@users.noreply.github.com>
2023-08-12 15:31:55 -0300

    Aula 09/08 - Inclui o model Post

--- blog/views.py
+++ blog/views.py
@@ -3,11 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from blog.models import Post # Acrescentar
 
 def index(request):
     # return HttpResponse('Olá Django - index')
     return render(request, 'index.html', {'titulo': 'Últimos Artigos'})
 
-def ola(request):
-    return render(request, 'home.html')
-
+def ola(request): # Modificar
+    # return HttpResponse('Olá django')
+    posts = Post.objects.all() # recupera todos os posts do banco de dados
+    context = {'posts_list': posts } # cria um dicionário com os dado
+    return render(request, 'posts.html', context) # renderiza o template e passa o contexto
