b74bc4e07740e671b0d29e3a15bd51bddf56af5e
Hyan Ferreira <hcgamesmg2016@gmail.com>
2023-08-16 11:00:11 -0300

    Aula 16/08 - Área Administrativa

--- webapp/settings.py
+++ webapp/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'django_extensions', # Acrescentar
 ]
 
 MIDDLEWARE = [
@@ -78,11 +79,14 @@ WSGI_APPLICATION = 'webapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_blog_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
